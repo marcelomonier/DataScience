4b0f590063b652d5ca862a47648c80e0c37684dc
Marcelo Monier <marcelomonier@gmail.com>
2021-09-16 00:17:48 -0300

    inclusão dashboard Stroke2

--- Stroke/dashboard.py
+++ Stroke/dashboard.py
@@ -22,7 +22,7 @@ df = pd.DataFrame({
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Monier'),
 
     html.Div(children='''
         Dash: A web application framework for your data.
